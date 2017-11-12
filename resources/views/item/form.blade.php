2dadacb165698b9435e0bef9d4b8051c6037be21
Satya Syahputra <SATYADARA69@gmail.com>
2017-11-12 22:37:36 +0700

    tampilan ok

--- resources/views/item/form.blade.php
+++ resources/views/item/form.blade.php
@@ -5,13 +5,22 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Form</div>
 
                 <div class="panel-body">
                     <form class="form-horizontal" method="post">
                         {{ csrf_field() }}
+                        <div class="form-group col-md-4">
+                            <input class="form-control" type="text" placeholder="Item Name" required>
+                        </div>
+                        <div class="form-group col-md-4">
+                            <input class="form-control" type="number" placeholder="Item Stock" required>
+                        </div>
+                        <div class="form-group col-md-4">
+                            <input class="form-control" type="number" placeholder="Item Price" required>
+                        </div>
                         <div class="form-group">
-                            <label class="control-label"></label>
+                            <button class="btn btn-primary" type="submit">Submit</button>
                         </div>
                     </form>
                 </div>
